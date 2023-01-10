059b4bd7f7df2e9318354f17d68b771a0b1b7ab7
Jorge Martínez <jmartineztortola@gmail.com>
2023-01-10 22:01:34 +0100

    Creating first models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateField()
+    
+    def __str__(self): 
+        return self.first_name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   title = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   inventory = models.SmallIntegerField()
+
+   def __str__(self):
+      return self.name
